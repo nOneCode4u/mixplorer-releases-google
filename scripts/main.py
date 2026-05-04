e80620d3c265de9a9ea6253a7d3fcc10f86faedc
No One <121163474+nOneCode4u@users.noreply.github.com>
2026-05-04 16:41:49 +0530

    Add files via upload

--- scripts/main.py
+++ scripts/main.py
@@ -112,60 +112,49 @@ def append_pending_overrides(entries: list[dict]) -> None:
     MANUAL_VERSIONS_FILE.write_text(content + "\n" + "\n".join(new_rows) + "\n", encoding="utf-8")
 
 
-def _generate_obtainium_deep_link(app_name: str, display_name: str) -> str:
-    filter_map = {
-        "MiXplorer":   "MiXplorer",
-        "MiX_Archive": "Archive",
-        "MiX_Codecs":  "Codecs",
-        "MiX_Encrypt": "Encrypt",
-        "MiX_Image":   "Image",
-        "MiX_PDF":     "PDF",
-        "MiX_Tagger":  "Tagger",
+def _get_obtainium_url(app_name: str, display_name: str) -> str:
+    """
+    Return the Obtainium deep link for a known app.
+    URLs sourced directly from README.md — identical to what users tap.
+    For unknown future apps, falls back to a generic repo link.
+    """
+    _URLS: dict[str, str] = {
+    "MiX_Archive": "https://apps.obtainium.imranr.dev/redirect?r=obtainium://app/%7B%22id%22%3A%20%22com.mixplorer.addon.archive%22%2C%22url%22%3A%20%22https%3A%2F%2Fgithub.com%2FnOneCode4u%2Fmixplorer-google-drive%22%2C%22author%22%3A%20%22nOneCode4u%22%2C%22name%22%3A%20%22MiX%20Archive%22%2C%22additionalSettings%22%3A%20%22%7B%5C%22includePrereleases%5C%22%3Afalse%2C%5C%22fallbackToOlderReleases%5C%22%3Atrue%2C%5C%22filterReleaseTitlesByRegEx%5C%22%3A%5C%22Archive%5C%22%2C%5C%22filterReleaseNotesByRegEx%5C%22%3A%5C%22%5C%22%2C%5C%22verifyLatestTag%5C%22%3Afalse%2C%5C%22sortMethodChoice%5C%22%3A%5C%22date%5C%22%2C%5C%22useLatestAssetDateAsReleaseDate%5C%22%3Afalse%2C%5C%22releaseTitleAsVersion%5C%22%3Afalse%2C%5C%22trackOnly%5C%22%3Afalse%2C%5C%22versionExtractionRegEx%5C%22%3A%5C%22%5C%22%2C%5C%22matchGroupToUse%5C%22%3A%5C%22%5C%22%2C%5C%22versionDetection%5C%22%3Atrue%2C%5C%22releaseDateAsVersion%5C%22%3Afalse%2C%5C%22useVersionCodeAsOSVersion%5C%22%3Afalse%2C%5C%22apkFilterRegEx%5C%22%3A%5C%22%5C%22%2C%5C%22invertAPKFilter%5C%22%3Afalse%2C%5C%22autoApkFilterByArch%5C%22%3Atrue%2C%5C%22appName%5C%22%3A%5C%22MiX%20Archive%5C%22%2C%5C%22appAuthor%5C%22%3A%5C%22nOneCode4u%5C%22%2C%5C%22shizukuPretendToBeGooglePlay%5C%22%3Atrue%2C%5C%22allowInsecure%5C%22%3Atrue%2C%5C%22exemptFromBackgroundUpdates%5C%22%3Afalse%2C%5C%22skipUpdateNotifications%5C%22%3Afalse%2C%5C%22about%5C%22%3A%5C%22Archive%20add-on%20%E2%80%94%20full%20create%2Fextract%20support%20for%20ZIP%2C%20RAR%2C%20RAR5%2C%207z%2C%20TAR%2C%20GZ%2C%20BZ2%2C%20XZ%2C%20ISO%20and%20more.%20Supports%20password-protected%20and%20multi-volume%20archives.%20Requires%20MiXplorer.%5C%22%2C%5C%22refreshBeforeDownload%5C%22%3Atrue%2C%5C%22includeZips%5C%22%3Afalse%2C%5C%22zippedApkFilterRegEx%5C%22%3A%5C%22%5C%22%7D%22%2C%22overrideSource%22%3A%20%22GitHub%22%2C%22allowIdChange%22%3A%20true%7D",
+    "MiX_Codecs": "https://apps.obtainium.imranr.dev/redirect?r=obtainium://app/%7B%22id%22%3A%20%22com.mixplorer.addon.codecs%22%2C%22url%22%3A%20%22https%3A%2F%2Fgithub.com%2FnOneCode4u%2Fmixplorer-google-drive%22%2C%22author%22%3A%20%22nOneCode4u%22%2C%22name%22%3A%20%22MiX%20Codecs%22%2C%22additionalSettings%22%3A%20%22%7B%5C%22includePrereleases%5C%22%3Afalse%2C%5C%22fallbackToOlderReleases%5C%22%3Atrue%2C%5C%22filterReleaseTitlesByRegEx%5C%22%3A%5C%22Codecs%5C%22%2C%5C%22filterReleaseNotesByRegEx%5C%22%3A%5C%22%5C%22%2C%5C%22verifyLatestTag%5C%22%3Afalse%2C%5C%22sortMethodChoice%5C%22%3A%5C%22date%5C%22%2C%5C%22useLatestAssetDateAsReleaseDate%5C%22%3Afalse%2C%5C%22releaseTitleAsVersion%5C%22%3Afalse%2C%5C%22trackOnly%5C%22%3Afalse%2C%5C%22versionExtractionRegEx%5C%22%3A%5C%22%5C%22%2C%5C%22matchGroupToUse%5C%22%3A%5C%22%5C%22%2C%5C%22versionDetection%5C%22%3Atrue%2C%5C%22releaseDateAsVersion%5C%22%3Afalse%2C%5C%22useVersionCodeAsOSVersion%5C%22%3Afalse%2C%5C%22apkFilterRegEx%5C%22%3A%5C%22%5C%22%2C%5C%22invertAPKFilter%5C%22%3Afalse%2C%5C%22autoApkFilterByArch%5C%22%3Atrue%2C%5C%22appName%5C%22%3A%5C%22MiX%20Codecs%5C%22%2C%5C%22appAuthor%5C%22%3A%5C%22nOneCode4u%5C%22%2C%5C%22shizukuPretendToBeGooglePlay%5C%22%3Atrue%2C%5C%22allowInsecure%5C%22%3Atrue%2C%5C%22exemptFromBackgroundUpdates%5C%22%3Afalse%2C%5C%22skipUpdateNotifications%5C%22%3Afalse%2C%5C%22about%5C%22%3A%5C%22Media%20codec%20add-on%20%E2%80%94%20extends%20MiXplorer%27s%20built-in%20player%20with%20additional%20audio%2Fvideo%20decoding%20for%20FLAC%2C%20OGG%2C%20MKV%2C%20AVI%20and%20formats%20not%20natively%20supported%20by%20Android.%20Requires%20MiXplorer.%5C%22%2C%5C%22refreshBeforeDownload%5C%22%3Atrue%2C%5C%22includeZips%5C%22%3Afalse%2C%5C%22zippedApkFilterRegEx%5C%22%3A%5C%22%5C%22%7D%22%2C%22overrideSource%22%3A%20%22GitHub%22%2C%22allowIdChange%22%3A%20true%7D",
+    "MiX_Encrypt": "https://apps.obtainium.imranr.dev/redirect?r=obtainium://app/%7B%22id%22%3A%20%22com.mixplorer.addon.encrypt%22%2C%22url%22%3A%20%22https%3A%2F%2Fgithub.com%2FnOneCode4u%2Fmixplorer-google-drive%22%2C%22author%22%3A%20%22nOneCode4u%22%2C%22name%22%3A%20%22MiX%20Encrypt%22%2C%22additionalSettings%22%3A%20%22%7B%5C%22includePrereleases%5C%22%3Afalse%2C%5C%22fallbackToOlderReleases%5C%22%3Atrue%2C%5C%22filterReleaseTitlesByRegEx%5C%22%3A%5C%22Encrypt%5C%22%2C%5C%22filterReleaseNotesByRegEx%5C%22%3A%5C%22%5C%22%2C%5C%22verifyLatestTag%5C%22%3Afalse%2C%5C%22sortMethodChoice%5C%22%3A%5C%22date%5C%22%2C%5C%22useLatestAssetDateAsReleaseDate%5C%22%3Afalse%2C%5C%22releaseTitleAsVersion%5C%22%3Afalse%2C%5C%22trackOnly%5C%22%3Afalse%2C%5C%22versionExtractionRegEx%5C%22%3A%5C%22%5C%22%2C%5C%22matchGroupToUse%5C%22%3A%5C%22%5C%22%2C%5C%22versionDetection%5C%22%3Atrue%2C%5C%22releaseDateAsVersion%5C%22%3Afalse%2C%5C%22useVersionCodeAsOSVersion%5C%22%3Afalse%2C%5C%22apkFilterRegEx%5C%22%3A%5C%22%5C%22%2C%5C%22invertAPKFilter%5C%22%3Afalse%2C%5C%22autoApkFilterByArch%5C%22%3Atrue%2C%5C%22appName%5C%22%3A%5C%22MiX%20Encrypt%5C%22%2C%5C%22appAuthor%5C%22%3A%5C%22nOneCode4u%5C%22%2C%5C%22shizukuPretendToBeGooglePlay%5C%22%3Atrue%2C%5C%22allowInsecure%5C%22%3Atrue%2C%5C%22exemptFromBackgroundUpdates%5C%22%3Afalse%2C%5C%22skipUpdateNotifications%5C%22%3Afalse%2C%5C%22about%5C%22%3A%5C%22Encryption%20add-on%20%E2%80%94%20AES-based%20file%20encryption%20and%20decryption%20directly%20inside%20MiXplorer.%20Manage%20encrypted%20containers%20without%20leaving%20the%20file%20manager.%20Requires%20MiXplorer.%5C%22%2C%5C%22refreshBeforeDownload%5C%22%3Atrue%2C%5C%22includeZips%5C%22%3Afalse%2C%5C%22zippedApkFilterRegEx%5C%22%3A%5C%22%5C%22%7D%22%2C%22overrideSource%22%3A%20%22GitHub%22%2C%22allowIdChange%22%3A%20true%7D",
+    "MiX_Image": "https://apps.obtainium.imranr.dev/redirect?r=obtainium://app/%7B%22id%22%3A%20%22com.mixplorer.addon.image%22%2C%22url%22%3A%20%22https%3A%2F%2Fgithub.com%2FnOneCode4u%2Fmixplorer-google-drive%22%2C%22author%22%3A%20%22nOneCode4u%22%2C%22name%22%3A%20%22MiX%20Image%22%2C%22additionalSettings%22%3A%20%22%7B%5C%22includePrereleases%5C%22%3Afalse%2C%5C%22fallbackToOlderReleases%5C%22%3Atrue%2C%5C%22filterReleaseTitlesByRegEx%5C%22%3A%5C%22Image%5C%22%2C%5C%22filterReleaseNotesByRegEx%5C%22%3A%5C%22%5C%22%2C%5C%22verifyLatestTag%5C%22%3Afalse%2C%5C%22sortMethodChoice%5C%22%3A%5C%22date%5C%22%2C%5C%22useLatestAssetDateAsReleaseDate%5C%22%3Afalse%2C%5C%22releaseTitleAsVersion%5C%22%3Afalse%2C%5C%22trackOnly%5C%22%3Afalse%2C%5C%22versionExtractionRegEx%5C%22%3A%5C%22%5C%22%2C%5C%22matchGroupToUse%5C%22%3A%5C%22%5C%22%2C%5C%22versionDetection%5C%22%3Atrue%2C%5C%22releaseDateAsVersion%5C%22%3Afalse%2C%5C%22useVersionCodeAsOSVersion%5C%22%3Afalse%2C%5C%22apkFilterRegEx%5C%22%3A%5C%22%5C%22%2C%5C%22invertAPKFilter%5C%22%3Afalse%2C%5C%22autoApkFilterByArch%5C%22%3Atrue%2C%5C%22appName%5C%22%3A%5C%22MiX%20Image%5C%22%2C%5C%22appAuthor%5C%22%3A%5C%22nOneCode4u%5C%22%2C%5C%22shizukuPretendToBeGooglePlay%5C%22%3Atrue%2C%5C%22allowInsecure%5C%22%3Atrue%2C%5C%22exemptFromBackgroundUpdates%5C%22%3Afalse%2C%5C%22skipUpdateNotifications%5C%22%3Afalse%2C%5C%22about%5C%22%3A%5C%22Image%20viewer%20add-on%20%E2%80%94%20wide%20format%20support%20including%20RAW%2C%20TIFF%2C%20and%20WEBP%2C%20with%20smooth%20zoom%2C%20pan%2C%20and%20gallery%20navigation.%20Includes%20basic%20editing%20and%20rotation.%20Requires%20MiXplorer.%5C%22%2C%5C%22refreshBeforeDownload%5C%22%3Atrue%2C%5C%22includeZips%5C%22%3Afalse%2C%5C%22zippedApkFilterRegEx%5C%22%3A%5C%22%5C%22%7D%22%2C%22overrideSource%22%3A%20%22GitHub%22%2C%22allowIdChange%22%3A%20true%7D",
+    "MiX_PDF": "https://apps.obtainium.imranr.dev/redirect?r=obtainium://app/%7B%22id%22%3A%20%22com.mixplorer.addon.pdf%22%2C%22url%22%3A%20%22https%3A%2F%2Fgithub.com%2FnOneCode4u%2Fmixplorer-google-drive%22%2C%22author%22%3A%20%22nOneCode4u%22%2C%22name%22%3A%20%22MiX%20PDF%22%2C%22additionalSettings%22%3A%20%22%7B%5C%22includePrereleases%5C%22%3Afalse%2C%5C%22fallbackToOlderReleases%5C%22%3Atrue%2C%5C%22filterReleaseTitlesByRegEx%5C%22%3A%5C%22PDF%5C%22%2C%5C%22filterReleaseNotesByRegEx%5C%22%3A%5C%22%5C%22%2C%5C%22verifyLatestTag%5C%22%3Afalse%2C%5C%22sortMethodChoice%5C%22%3A%5C%22date%5C%22%2C%5C%22useLatestAssetDateAsReleaseDate%5C%22%3Afalse%2C%5C%22releaseTitleAsVersion%5C%22%3Afalse%2C%5C%22trackOnly%5C%22%3Afalse%2C%5C%22versionExtractionRegEx%5C%22%3A%5C%22%5C%22%2C%5C%22matchGroupToUse%5C%22%3A%5C%22%5C%22%2C%5C%22versionDetection%5C%22%3Atrue%2C%5C%22releaseDateAsVersion%5C%22%3Afalse%2C%5C%22useVersionCodeAsOSVersion%5C%22%3Afalse%2C%5C%22apkFilterRegEx%5C%22%3A%5C%22%5C%22%2C%5C%22invertAPKFilter%5C%22%3Afalse%2C%5C%22autoApkFilterByArch%5C%22%3Atrue%2C%5C%22appName%5C%22%3A%5C%22MiX%20PDF%5C%22%2C%5C%22appAuthor%5C%22%3A%5C%22nOneCode4u%5C%22%2C%5C%22shizukuPretendToBeGooglePlay%5C%22%3Atrue%2C%5C%22allowInsecure%5C%22%3Atrue%2C%5C%22exemptFromBackgroundUpdates%5C%22%3Afalse%2C%5C%22skipUpdateNotifications%5C%22%3Afalse%2C%5C%22about%5C%22%3A%5C%22PDF%20viewer%20add-on%20%E2%80%94%20render%20and%20navigate%20PDF%20documents%20of%20any%20length%20directly%20inside%20MiXplorer.%20Supports%20text%20selection%2C%20zoom%2C%20and%20multi-page%20navigation.%20Requires%20MiXplorer.%5C%22%2C%5C%22refreshBeforeDownload%5C%22%3Atrue%2C%5C%22includeZips%5C%22%3Afalse%2C%5C%22zippedApkFilterRegEx%5C%22%3A%5C%22%5C%22%7D%22%2C%22overrideSource%22%3A%20%22GitHub%22%2C%22allowIdChange%22%3A%20true%7D",
+    "MiX_Tagger": "https://apps.obtainium.imranr.dev/redirect?r=obtainium://app/%7B%22id%22%3A%20%22com.mixplorer.addon.tagger%22%2C%22url%22%3A%20%22https%3A%2F%2Fgithub.com%2FnOneCode4u%2Fmixplorer-google-drive%22%2C%22author%22%3A%20%22nOneCode4u%22%2C%22name%22%3A%20%22MiX%20Tagger%22%2C%22additionalSettings%22%3A%20%22%7B%5C%22includePrereleases%5C%22%3Afalse%2C%5C%22fallbackToOlderReleases%5C%22%3Atrue%2C%5C%22filterReleaseTitlesByRegEx%5C%22%3A%5C%22Tagger%5C%22%2C%5C%22filterReleaseNotesByRegEx%5C%22%3A%5C%22%5C%22%2C%5C%22verifyLatestTag%5C%22%3Afalse%2C%5C%22sortMethodChoice%5C%22%3A%5C%22date%5C%22%2C%5C%22useLatestAssetDateAsReleaseDate%5C%22%3Afalse%2C%5C%22releaseTitleAsVersion%5C%22%3Afalse%2C%5C%22trackOnly%5C%22%3Afalse%2C%5C%22versionExtractionRegEx%5C%22%3A%5C%22%5C%22%2C%5C%22matchGroupToUse%5C%22%3A%5C%22%5C%22%2C%5C%22versionDetection%5C%22%3Atrue%2C%5C%22releaseDateAsVersion%5C%22%3Afalse%2C%5C%22useVersionCodeAsOSVersion%5C%22%3Afalse%2C%5C%22apkFilterRegEx%5C%22%3A%5C%22%5C%22%2C%5C%22invertAPKFilter%5C%22%3Afalse%2C%5C%22autoApkFilterByArch%5C%22%3Atrue%2C%5C%22appName%5C%22%3A%5C%22MiX%20Tagger%5C%22%2C%5C%22appAuthor%5C%22%3A%5C%22nOneCode4u%5C%22%2C%5C%22shizukuPretendToBeGooglePlay%5C%22%3Atrue%2C%5C%22allowInsecure%5C%22%3Atrue%2C%5C%22exemptFromBackgroundUpdates%5C%22%3Afalse%2C%5C%22skipUpdateNotifications%5C%22%3Afalse%2C%5C%22about%5C%22%3A%5C%22Audio%20tag%20editor%20add-on%20%E2%80%94%20view%20and%20edit%20ID3v1%2C%20ID3v2%2C%20Vorbis%20Comment%2C%20APEv2%20and%20other%20tag%20formats%20for%20MP3%2C%20FLAC%2C%20OGG%2C%20M4A%2C%20OPUS%20and%20more.%20Supports%20batch%20editing%20and%20artwork.%20Requires%20MiXplorer.%5C%22%2C%5C%22refreshBeforeDownload%5C%22%3Atrue%2C%5C%22includeZips%5C%22%3Afalse%2C%5C%22zippedApkFilterRegEx%5C%22%3A%5C%22%5C%22%7D%22%2C%22overrideSource%22%3A%20%22GitHub%22%2C%22allowIdChange%22%3A%20true%7D",
+    "MiXplorer": "https://apps.obtainium.imranr.dev/redirect?r=obtainium://app/%7B%22id%22%3A%20%22com.mixplorer%22%2C%22url%22%3A%20%22https%3A%2F%2Fgithub.com%2FnOneCode4u%2Fmixplorer-google-drive%22%2C%22author%22%3A%20%22nOneCode4u%22%2C%22name%22%3A%20%22MiXplorer%22%2C%22additionalSettings%22%3A%20%22%7B%5C%22includePrereleases%5C%22%3Afalse%2C%5C%22fallbackToOlderReleases%5C%22%3Atrue%2C%5C%22filterReleaseTitlesByRegEx%5C%22%3A%5C%22MiXplorer%5C%22%2C%5C%22filterReleaseNotesByRegEx%5C%22%3A%5C%22%5C%22%2C%5C%22verifyLatestTag%5C%22%3Afalse%2C%5C%22sortMethodChoice%5C%22%3A%5C%22date%5C%22%2C%5C%22useLatestAssetDateAsReleaseDate%5C%22%3Afalse%2C%5C%22releaseTitleAsVersion%5C%22%3Afalse%2C%5C%22trackOnly%5C%22%3Afalse%2C%5C%22versionExtractionRegEx%5C%22%3A%5C%22%5C%22%2C%5C%22matchGroupToUse%5C%22%3A%5C%22%5C%22%2C%5C%22versionDetection%5C%22%3Atrue%2C%5C%22releaseDateAsVersion%5C%22%3Afalse%2C%5C%22useVersionCodeAsOSVersion%5C%22%3Afalse%2C%5C%22apkFilterRegEx%5C%22%3A%5C%22%5C%22%2C%5C%22invertAPKFilter%5C%22%3Afalse%2C%5C%22autoApkFilterByArch%5C%22%3Atrue%2C%5C%22appName%5C%22%3A%5C%22MiXplorer%5C%22%2C%5C%22appAuthor%5C%22%3A%5C%22nOneCode4u%5C%22%2C%5C%22shizukuPretendToBeGooglePlay%5C%22%3Atrue%2C%5C%22allowInsecure%5C%22%3Atrue%2C%5C%22exemptFromBackgroundUpdates%5C%22%3Afalse%2C%5C%22skipUpdateNotifications%5C%22%3Afalse%2C%5C%22about%5C%22%3A%5C%22Fast%2C%20smooth%2C%20dual-pane%20file%20manager%20with%20cloud%20storage%2C%20archive%20support%2C%20built-in%20media%20player%2C%20and%20deep%20customisation.%20No%20ads%2C%20no%20trackers.%5C%22%2C%5C%22refreshBeforeDownload%5C%22%3Atrue%2C%5C%22includeZips%5C%22%3Afalse%2C%5C%22zippedApkFilterRegEx%5C%22%3A%5C%22%5C%22%7D%22%2C%22overrideSource%22%3A%20%22GitHub%22%2C%22allowIdChange%22%3A%20true%7D",
     }
-    pkg_map = {
-        "MiXplorer":   "com.mixplorer",
-        "MiX_Archive": "com.mixplorer.addon.archive",
-        "MiX_Codecs":  "com.mixplorer.addon.codecs",
-        "MiX_Encrypt": "com.mixplorer.addon.encrypt",
-        "MiX_Image":   "com.mixplorer.addon.image",
-        "MiX_PDF":     "com.mixplorer.addon.pdf",
-        "MiX_Tagger":  "com.mixplorer.addon.tagger",
-    }
-    pkg_id       = pkg_map.get(app_name, f"com.mixplorer.{app_name.lower()}")
-    title_filter = filter_map.get(app_name, app_name)
-    config = {
-        "id":     pkg_id,
+    if app_name in _URLS:
+        return _URLS[app_name]
+    # Future unknown add-on fallback — opens repo in Obtainium
+    import urllib.parse, json as _json
+    cfg = {
+        "id":     f"com.mixplorer.{app_name.lower().replace('_', '.')}",
         "url":    f"https://github.com/{GH_REPO}",
         "author": "nOneCode4u",
         "name":   display_name,
-        "additionalSettings": json.dumps({
-            "includePrereleases":           False,
-            "fallbackToOlderReleases":      True,
-            "filterReleaseTitlesByRegEx":   title_filter,
-            "filterReleaseNotesByRegEx":    "",
-            "verifyLatestTag":              False,
-            "sortMethodChoice":             "date",
-            "autoApkFilterByArch":          True,
-            "apkFilterRegEx":               "",
-            "invertAPKFilter":              False,
-            "shizukuPretendToBeGooglePlay": True,
-            "allowInsecure":                True,
-            "refreshBeforeDownload":        True,
+        "additionalSettings": _json.dumps({
+            "includePrereleases":         False,
+            "fallbackToOlderReleases":    True,
+            "filterReleaseTitlesByRegEx": app_name.split("_")[-1] if "_" in app_name else app_name,
+            "sortMethodChoice":           "date",
+            "autoApkFilterByArch":        True,
+            "refreshBeforeDownload":      True,
         }),
         "overrideSource": "GitHub",
         "allowIdChange":  True,
     }
-    encoded = urllib.parse.quote(json.dumps(config, separators=(",", ":")), safe="")
-    return f"https://apps.obtainium.imranr.dev/redirect?r=obtainium://app/{encoded}"
-
-
-
+    return ("https://apps.obtainium.imranr.dev/redirect?r=obtainium://app/"
+            + urllib.parse.quote(_json.dumps(cfg, separators=(",", ":")), safe=""))
 
 def build_release_body(app_name: str, version_name: str, descriptions: dict, changelog: Optional[str]) -> str:
     info          = descriptions.get(app_name, {})
     display       = info.get("display_name", get_display_name(app_name))
     icon          = info.get("icon", "📦")
-    obtainium_url = _generate_obtainium_deep_link(app_name, display)
+    obtainium_url = _get_obtainium_url(app_name, display)
 
     if changelog:
         middle = f"### What's New\n\n{changelog}\n\n---\n\n"
